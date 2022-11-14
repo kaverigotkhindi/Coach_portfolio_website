406ba104acdeeb1a99fea04a4ca2996177f49fe1
Kaveri <kaverisg.tce@gmail.com>
2022-11-14 20:49:28 +0530

    adding results page

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+var btn = document.getElementById('click');
+btn.addEventListener('click',function onClick(){
+    console.log('clicked');
+})
+
 var activePage = window.location.pathname;
 
 var navLinks = document.querySelectorAll('nav li a').forEach(link => {
    
     if(link.href.includes(`${activePage}`))
     link.classList.add('active');
-    console.log(link+'  link');
-    console.log(link.href+'  link.href');
-    console.log(`${activePage}`+'  activePage')
-    console.log(activePage+'  activePage');
+   
 })
