0334e97b35a39856f662985d45cc492799adadcf
Kaveri <kaverisg.tce@gmail.com>
2022-11-16 15:37:54 +0530

    worked on the feedback

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var btn = document.getElementById('click');
-btn.addEventListener('click',function onClick(){
-    console.log('clicked');
-})
+// var btn = document.getElementById('click');
+// btn.addEventListener('click',function onClick(){
+//     console.log('clicked');
+// })
 
 var activePage = window.location.pathname;
-
+console.log(activePage);
 var navLinks = document.querySelectorAll('nav li a').forEach(link => {
-   
+    
     if(link.href.includes(`${activePage}`))
     link.classList.add('active');
+    console.log(activePage);
    
 })
